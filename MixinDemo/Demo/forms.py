f10423b6684061ec43150575429ad456d0ebe542
Abdurrazak Muhammad <99066990+mohdabduraz@users.noreply.github.com>
2022-12-13 13:19:46 -0800

    Update forms.py

--- MixinDemo/Demo/forms.py
+++ MixinDemo/Demo/forms.py
@@ -3,10 +3,16 @@ from django import forms
 # Create your tests here.
 
 class Loginform(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+
+        widget = forms.TextInput(attrs = {"class":"loginstyle"})
+        pwdstyle = forms.PasswordInput(attrs = {"class":"loginstyle1"})
+
+        super().__init__(*args, **kwargs)
+
+        self.fields["Username"] = forms.CharField(widget=widget)
+        self.fields["Password"] = forms.CharField(widget=pwdstyle)
+        
     template_name_label = None
-    widget = forms.TextInput(attrs = {"class":"loginstyle"})
-    Username = forms.CharField(widget=widget)
-    pwstyle = forms.PasswordInput()
-    pwstyle.attrs["class"] = "loginstyle1"
-    Password = forms.CharField(widget=pwstyle)
-    
+
